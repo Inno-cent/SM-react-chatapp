c8da8cc05b4e607a8f0294089837127427e2f488
innocent <ekweozorinnocent@gmail.com>
2024-06-03 20:55:54 +0100

    server update

--- Backend/index.js
+++ Backend/index.js
@@ -4,17 +4,23 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
-
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
