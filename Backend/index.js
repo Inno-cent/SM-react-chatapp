00496485dea128ac160cbd5c8915bb4d559db30c
innocent <ekweozorinnocent@gmail.com>
2024-06-03 20:54:51 +0100

    server update

--- Backend/index.js
+++ Backend/index.js
@@ -3,3 +3,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
+
+dotenv.config()
+
+const app = express();
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
