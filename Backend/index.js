dcc832c8ac3514af2b2ad1b9a623af27bbedd3d2
innocent <ekweozorinnocent@gmail.com>
2024-06-03 20:57:01 +0100

    server update

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2121;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
