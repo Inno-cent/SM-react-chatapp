6bc682f8f79c5ee7b5995c04973565ac81ae3e78
innocent <ekweozorinnocent@gmail.com>
2024-06-05 11:02:06 +0100

    update

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -6,7 +6,7 @@ export const signup = async (req, res) => {
   const { fullname, email, password, confirmPassword } = req.body;
 
   try {
-    if (password != confirmPassword) {
+    if (password !== confirmPassword) {
       return res.status(400).json({ error: "Password does not match" });
     }
 
@@ -19,11 +19,23 @@ export const signup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await new User({
-        fullname,
-        email,
-        password: hashedPassword
-    })
+      fullname,
+      email,
+      password: hashedPassword,
+    });
 
-    await newUser.save()
+    await newUser.save();
+
+    if (newUser) {
+      CTASC(newUser._id, res);
+      res.status(201).json({
+        message: " user created successfully",
+        user: {
+          _id: newUser._id,
+          fullname: newUser.fullname,
+          email: newUser.email,
+        },
+      });
+    }
   } catch {}
 };
