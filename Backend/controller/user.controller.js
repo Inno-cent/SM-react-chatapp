55bae1ad30941b0cf88543caecfcfaa8dd252708
innocent <ekweozorinnocent@gmail.com>
2024-06-05 10:50:27 +0100

    update

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -5,12 +5,23 @@ import CTASC from "../jwt/generateToken.js";
 export const signup = async (req, res) => {
   const { fullname, email, password, confirmPassword } = req.body;
 
-  try{
-    if(password != confirmPassword){
-        return res.status(400).json({ error : "Password does not match"})
+  try {
+    if (password != confirmPassword) {
+      return res.status(400).json({ error: "Password does not match" });
     }
-  }
-  catch{
-    
-  }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "User already exist" });
+    }
+
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await new User({
+        fullname,
+        email,
+        password: hashedPassword
+    })
+  } catch {}
 };
