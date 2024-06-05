08e58483ecaa5f749efc1f6181b74166f4e16474
innocent <ekweozorinnocent@gmail.com>
2024-06-05 10:42:35 +0100

    update

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -4,4 +4,13 @@ import CTASC from "../jwt/generateToken.js";
 
 export const signup = async (req, res) => {
   const { fullname, email, password, confirmPassword } = req.body;
+
+  try{
+    if(password != confirmPassword){
+        return res.status(400).json({ error : "Password does not match"})
+    }
+  }
+  catch{
+    
+  }
 };
