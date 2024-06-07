1a4d2781f69149a4754dc48921ea8eb9561a0309
innocent <ekweozorinnocent@gmail.com>
2024-06-08 00:25:16 +0100

    context

--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -31,7 +31,7 @@ function Signup() {
             >
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
-            <input type="text" className="grow" placeholder="Fullname" />
+            <input type="text" className="grow" placeholder="Fullname" value={fullname} onChange={(e) => setFullname(e.target.value)} />
           </label>
 
           {/* Email */}
@@ -45,7 +45,7 @@ function Signup() {
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
-            <input type="email" className="grow" placeholder="Email" />
+            <input type="email" className="grow" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </label>
 
           {/* Password */}
@@ -62,7 +62,7 @@ function Signup() {
                 clipRule="evenodd"
               />
             </svg>
-            <input type="password" className="grow" placeholder="password" />
+            <input type="password" className="grow" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </label>
 
           {/*Confirm Password */}
@@ -83,6 +83,7 @@ function Signup() {
               type="password"
               className="grow"
               placeholder="confirm password"
+              value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </label>
 
