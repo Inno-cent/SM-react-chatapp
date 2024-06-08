6b58cb3ef8f76fceba7b7280959b2a5b27112217
innocent <ekweozorinnocent@gmail.com>
2024-06-08 21:17:42 +0100

    update on auth context

--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -1,25 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
+// import { AuthContext } from "../../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
 
-function Signup() {
-  const { signup } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [fullname, setFullname] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+const Signup = () => {
+  const [authUser, setAuthUser] = useAuth();
+  // const { signup } = useContext(AuthContext);
+  // const [email, setEmail] = useState('');
+  // const [fullname, setFullname] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    signup(email, password, fullname, confirmPassword);
-};
+  const onSubmit = async (data) => {
+    const userInfo = {
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+      confirmPassword: data.confirmPassword,
+    };
+    // console.log(userInfo);
+    await axios
+      .post("/api/user/signup", userInfo)
+      .then((response) => {
+        if (response.data) {
+          toast.success("Signup successful");
+        }
+        localStorage.setItem("ChatApp", JSON.stringify(response.data));
+        setAuthUser(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error("Error: " + error.response.data.error);
+        }
+      });
+  };
 
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <form className="bpx-6 py-2  space-y-3 w-96">
-          
           <h2 className="text-2xl text-white font-bold">Signup</h2>
           <br />
           {/* Fullname */}
@@ -32,7 +52,13 @@ function Signup() {
             >
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
-            <input type="text" className="grow" placeholder="Fullname" value={fullname} onChange={(e) => setFullname(e.target.value)} />
+            <input
+              type="text"
+              className="grow"
+              placeholder="Fullname"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+            />
           </label>
 
           {/* Email */}
@@ -46,7 +72,13 @@ function Signup() {
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
-            <input type="email" className="grow" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input
+              type="email"
+              className="grow"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
 
           {/* Password */}
@@ -63,7 +95,13 @@ function Signup() {
                 clipRule="evenodd"
               />
             </svg>
-            <input type="password" className="grow" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input
+              type="password"
+              className="grow"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
 
           {/*Confirm Password */}
@@ -84,7 +122,8 @@ function Signup() {
               type="password"
               className="grow"
               placeholder="confirm password"
-              value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </label>
 
@@ -99,8 +138,8 @@ function Signup() {
                 Login
               </Link>
             </p>
-            
-             <input
+
+            <input
               type="submit"
               value="Signup"
               className="text-white bg-green-500 px-8 py-2 cursor-pointer rounded-lg"
@@ -110,6 +149,6 @@ function Signup() {
       </div>
     </>
   );
-}
+};
 
 export default Signup;
