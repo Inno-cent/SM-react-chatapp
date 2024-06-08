3d32bda8ae93e48ab55fe899488b3c691547e20e
innocent <ekweozorinnocent@gmail.com>
2024-06-08 21:06:37 +0100

    update on auth context

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,56 +1,73 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+// const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Check if user is already logged in (e.g., check localStorage or a session)
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-    setLoading(false);
-  }, []);
+//   useEffect(() => {
+//     // Check if user is already logged in (e.g., check localStorage or a session)
+//     const storedUser = localStorage.getItem("user");
+//     if (storedUser) {
+//       setUser(JSON.parse(storedUser));
+//     }
+//     setLoading(false);
+//   }, []);
 
-  const signup = async (fullname, email, password, confirmPassword) => {
-    try {
-      const response = await axios.post("/api/user/signup", {
-        email,
-        password,
-        fullname,
-        confirmPassword,
-      });
-      setUser(response.data.user);
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-    } catch (error) {
-      console.error("Signup error:", error);
-    }
-  };
+//   const signup = async (fullname, email, password, confirmPassword) => {
+//     try {
+//       const response = await axios.post("/api/user/signup", {
+//         email,
+//         password,
+//         fullname,
+//         confirmPassword,
+//       });
+//       setUser(response.data.user);
+//       localStorage.setItem("user", JSON.stringify(response.data.user));
+//     } catch (error) {
+//       console.error("Signup error:", error);
+//     }
+//   };
 
-  const login = async (email, password) => {
-    try {
-      const response = await axios.post("/api/user/login", { email, password });
-      setUser(response.data.user);
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-    } catch (error) {
-      console.error("Login error:", error);
-    }
-  };
+//   const login = async (email, password) => {
+//     try {
+//       const response = await axios.post("/api/user/login", { email, password });
+//       setUser(response.data.user);
+//       localStorage.setItem("user", JSON.stringify(response.data.user));
+//     } catch (error) {
+//       console.error("Login error:", error);
+//     }
+//   };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-  };
+//   const logout = () => {
+//     setUser(null);
+//     localStorage.removeItem("user");
+//   };
 
+//   return (
+//     <AuthContext.Provider value={{ user, signup, login, logout, loading }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export { AuthProvider, AuthContext };
+
+export const AuthProvider = ({ children }) => {
+  const initialUserState =
+    Cookies.get("jwt") || localStorage.getItem("ChatApp");
+
+  // parse the user data and storing in state.
+  const [authUser, setAuthUser] = useState(
+    initialUserState ? JSON.parse(initialUserState) : undefined
+  );
   return (
-    <AuthContext.Provider value={{ user, signup, login, logout, loading }}>
+    <AuthContext.Provider value={[authUser, setAuthUser]}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthProvider, AuthContext };
+export const useAuth = () => useContext(AuthContext);
